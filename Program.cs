dd334c34c9b59c58f872bd93cec5d6067d25a0af
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-17 15:51:19 +0530

    MultiLevel Inheritance

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static MultiLevelInheritanceDemo.models.MultiLevel;
+
+namespace MultiLevelInheritanceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City myCity = new City();
+            myCity.Name();
+            myCity.Capital();
+            myCity.Food();
+        }
+    }
+}
---
+++ models/MultiLevel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiLevelInheritanceDemo.models
+{
+    internal class MultiLevel
+    {
+        public class Country
+        {
+            public void Name()
+            {
+                Console.WriteLine("The Name of the country is India");
+
+            }
+        }
+
+        public class State: Country {
+        
+            public void Capital()
+            {
+                Console.WriteLine("The Capital of the state Maharashtra is Mumbai");
+            }
+        }
+
+        public class City:State
+        {
+            public void Food()
+            {
+                Console.WriteLine("The famous food is Vada Pav");
+            }
+        }
+    }
+}
